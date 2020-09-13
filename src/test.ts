4b41ccb075f004b4e775e574dafeec3d574ef7b5
朗明 <langming.qzl@antgroup.com>
2020-09-13 17:52:48 +0800

    添加常量方案

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,11 @@
-import { get } from 'a'
+import { get, get2 } from 'a'
 
 get(['a', 'b', 'c'], ([a, b, c]) => {
+  console.log(a.aa, b.bb, c.cc)
+})
+
+
+get2(['a', 'b', 'c'] as const, ([a, b, c]) => {
   console.log(a.aa, b.bb, c.cc)
 })
 
